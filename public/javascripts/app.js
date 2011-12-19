01d5522f483de7f4d05bb3cdb67e1be13cb5fc82
Antonio Terreno <antonio.terreno@gmail.com>
2011-12-19 04:31:56 +0200

    doesnt send progress to the client but proper event chain

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,40 +1,30 @@
 var socket = io.connect();
 
-var logmsg = function(msg){ 
+var logmsg = function(msg) { 
   document.getElementById('msgbox').innerHTML += msg;
 };
-
 
 socket.on('connect', function(){
   logmsg('<b>Connect!</b>');
   socket.send('ping');
 });
 
-socket.on('message', function(message){
-    console.log(message);
-  logmsg('Message: ' + JSON.stringify(message));
-});
-
 socket.on('progress', function (data) {
     console.log(data);
     $("#progress").text("Progress: " + data.percent + '%');    
 });
-    
-
 
  $(document).ready(function() {
 
-    socket.on('message', function(message){
+  socket.on('message', function(message){
     console.log(message);
-    logmsg('Message: ' + JSON.stringify(message));
-    }); 
+    logmsg('Message: ' + message);
+  }); 
   
   $('#sendmsg').click(function() { 
       console.log("sendmsg"); 
 
       socket.emit('ping');
-
-
 
       socket.send('ping'); 
   });  
