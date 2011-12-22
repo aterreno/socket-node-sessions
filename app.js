46627460c69cf8c9bb903e1ac763db3322dd847a
Antonio Terreno <antonio.terreno@gmail.com>
2011-12-22 14:33:21 +0000

    works ok for me

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express'),
     form = require('connect-form'),
     app = express.createServer(form({ keepExtensions: true })),    
-    parseCookie = require('connect').utils.parseCookie,
-    progressEvent = new require('./progressEvent').ProgressEvent(),    
-    http = require('http'),
     Pusher = require('node-pusher'),
     pusher = new Pusher({
       appId: '12424',
@@ -15,14 +12,13 @@ var socket_id = '1302.1081607';
 
 app.configure(function () {
     pusher.auth(socket_id, 'sockets');
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.cookieParser());
+    app.use(express.static(__dirname + '/public'));    
 });
  
-app.listen(3000);
+app.listen(80);
 
 app.get('/', function(req, res) {
-  res.render('index.jade', { title: 'La la la' });
+  res.render('index.jade', { title: 'Sockets with pusher' });
 });
 
 app.post('/upload', function(req, res, next){
@@ -38,6 +34,6 @@ app.post('/upload', function(req, res, next){
 
   req.form.on('progress', function(bytesReceived, bytesExpected) {
     var percent = (bytesReceived / bytesExpected * 100) | 0;
-    pusher.trigger('sockets', 'mesage', { from: 'Jaewoong', content: percent }, socket_id, function(err, req, res) {});    
+    pusher.trigger('sockets', 'message', percent, socket_id, function(err, req, res) {});
   });
 });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,7 @@
 $(document).ready(function() {
-  // Enable pusher logging - don't include this in production
-  Pusher.log = function(message) {
-    if (window.console && window.console.log) window.console.log(message);
-  };
-
-  // Flash fallback logging - don't include this in production
-  WEB_SOCKET_DEBUG = true;
-
   var pusher = new Pusher('7d718e13067550e5ad49');
   var channel = pusher.subscribe('sockets');
   channel.bind('message', function(data) {
-    alert(data);
-  }); 
+    $("#progress").text("Progress:" + data + "%");
+  });
 });
-
