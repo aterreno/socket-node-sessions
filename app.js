a4bc34b353e455340e91795d6ed689cac17f2915
Antonio Terreno <antonio.terreno@gmail.com>
2011-12-22 14:14:13 +0000

    works well with pusher with no hassles fuck off socket.io

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ var express = require('express'),
     app = express.createServer(form({ keepExtensions: true })),    
     parseCookie = require('connect').utils.parseCookie,
     progressEvent = new require('./progressEvent').ProgressEvent(),    
-    http = require('http');
- 
+    http = require('http'),
+    Pusher = require('node-pusher'),
+    pusher = new Pusher({
+      appId: '12424',
+      key: '7d718e13067550e5ad49',
+      secret: '5b03a57e8cef7810aff7'
+    });
+        
+var socket_id = '1302.1081607';
+
 app.configure(function () {
+    pusher.auth(socket_id, 'sockets');
     app.use(express.static(__dirname + '/public'));
     app.use(express.cookieParser());
 });
@@ -29,6 +38,6 @@ app.post('/upload', function(req, res, next){
 
   req.form.on('progress', function(bytesReceived, bytesExpected) {
     var percent = (bytesReceived / bytesExpected * 100) | 0;
-    console.log(percent);
+    pusher.trigger('sockets', 'mesage', { from: 'Jaewoong', content: percent }, socket_id, function(err, req, res) {});    
   });
 });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,16 @@
-// Enable pusher logging - don't include this in production
-Pusher.log = function(message) {
-  if (window.console && window.console.log) window.console.log(message);
-};
+$(document).ready(function() {
+  // Enable pusher logging - don't include this in production
+  Pusher.log = function(message) {
+    if (window.console && window.console.log) window.console.log(message);
+  };
 
-// Flash fallback logging - don't include this in production
-WEB_SOCKET_DEBUG = true;
+  // Flash fallback logging - don't include this in production
+  WEB_SOCKET_DEBUG = true;
 
-var pusher = new Pusher('7d718e13067550e5ad49');
-var channel = pusher.subscribe('test_channel');
-channel.bind('my_event', function(data) {
-  alert(data);
+  var pusher = new Pusher('7d718e13067550e5ad49');
+  var channel = pusher.subscribe('sockets');
+  channel.bind('message', function(data) {
+    alert(data);
+  }); 
 });
 
- $(document).ready(function() {};
